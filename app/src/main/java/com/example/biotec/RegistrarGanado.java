4cbbfda6c78f6879416a056c04fdf1114ea360f0
Renzo <rbloor97@hotmail.com>
2022-01-30 23:19:04 -0500

    firebase agregado

--- app/src/main/java/com/example/biotec/RegistrarGanado.java
+++ app/src/main/java/com/example/biotec/RegistrarGanado.java
@@ -1,7 +1,46 @@
 package com.example.biotec;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class RegistrarGanado extends Fragment {
+import static android.content.ContentValues.TAG;
+
+public class RegistrarGanado extends AppCompatActivity {
+
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_registrar_ganado);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Ganado");
+        myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+    }
+
 
 }
--- app/src/main/java/com/example/biotec/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/biotec/ui/home/HomeFragment.java
@@ -1,21 +1,26 @@
 package com.example.biotec.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.biotec.RegistrarGanado;
 import com.example.biotec.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private LinearLayout ll_registrar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +32,15 @@ public class HomeFragment extends Fragment {
 
         //final TextView textView = binding.textHome;
         CardView cv_consulta = binding.cvConsulta;
+        ll_registrar = binding.llRegistrar;
+
+        ll_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), RegistrarGanado.class);
+                getContext().startActivity(intent);
+            }
+        });
         /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
